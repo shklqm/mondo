480806e15cf19658c4ad0057457cf1051928f230
Shkelqim Memolla <shkelqim.memolla@gmail.com>
2019-08-07 18:39:49 +0300

    updated urls with new views

--- mondis/mondis/urls.py
+++ mondis/mondis/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from api.views import AddPerson, AddSlot, EventList
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/person/', AddPerson.as_view()),
+    path('api/slot/', AddSlot.as_view()),
+    path('api/event/list', EventList.as_view()),
 ]
